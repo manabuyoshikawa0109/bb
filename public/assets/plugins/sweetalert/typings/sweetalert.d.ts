76da6202c4b01332729a91507c9971f447d3a501
吉川学 <yoshikawa.manabu@joydea.jp>
2022-04-16 18:55:10 +0900

    sweetalert2プラグインの不要なファイルを削除 セッション値を受け取りsweetalert2でアラート表示をするコンポーネントを追加

--- public/assets/plugins/sweetalert/typings/sweetalert.d.ts
+++
@@ -1,8 +0,0 @@
-import { SweetAlert } from "./core";
-
-declare global {
-  const sweetAlert: SweetAlert;
-}
-declare const swal: SweetAlert;
-export default swal;
-export as namespace swal;
---
+++ resources/views/admin/commons/components/js/sweetalert.blade.php
@@ -0,0 +1,23 @@
+{{--
+    アラート表示用コンポーネント
+    セッション（フラッシュ）にmessageが設定されていれば表示
+--}}
+
+@if(session('message'))
+@push('links')
+<link href="/assets/plugins/sweetalert2/sweetalert2.all.min.css" rel="stylesheet">
+@endpush
+
+@push('scripts')
+{{-- フラッシュメッセージ表示--}}
+<script src="/assets/plugins/sweetalert2/sweetalert2.all.min.js"></script>
+<script type="text/javascript">
+$(function(){
+    Swal.fire({
+        icon: 'success',
+        title: '{{ session('message') }}',
+    })
+});
+</script>
+@endpush
+@endif
