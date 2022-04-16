0b075a235048271ca49b03c15f2fff96a24f5de6
吉川学 <yoshikawa.manabu@joydea.jp>
2022-04-16 17:44:56 +0900

    fontawesomeのバージョンを4から6に変更

--- resources/views/admin/layouts/base.blade.php
+++ resources/views/admin/layouts/base.blade.php
@@ -6,15 +6,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="images/favicon.ico" type="image/ico" />
+    <link rel="icon" href="/assets/common/images/favicon.ico" type="image/ico" />
 
     <title>Gentelella Alela! | </title>
 
     <!-- Bootstrap -->
     <link href="/assets/plugins/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
-    <!-- <link href="/assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet"> -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" rel="stylesheet">
+    <link href="/assets/plugins/font-awesome/css/all.min.css" rel="stylesheet">
     <!-- NProgress -->
     <link href="/assets/plugins/nprogress/nprogress.css" rel="stylesheet">
     <!-- iCheck -->
