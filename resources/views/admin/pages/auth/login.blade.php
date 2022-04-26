1d9c4ff069e1da80b777b42ccc35fe13522d11d9
吉川学 <yoshikawa.manabu@joydea.jp>
2022-04-26 21:21:38 +0900

    管理者用ログイン画面の不要な処理を削除

--- resources/views/admin/pages/auth/login.blade.php
+++ resources/views/admin/pages/auth/login.blade.php
@@ -4,9 +4,6 @@
 
 @section('inner_body')
 <div>
-    <a class="hiddenanchor" id="signup"></a>
-    <a class="hiddenanchor" id="signin"></a>
-
     <div class="login_wrapper">
         <div class="animate form login_form">
             <section class="login_content">
@@ -20,59 +17,17 @@
                         <input type="password" name="password" class="form-control" placeholder="*******" required autocomplete="current-password" />
                     </div>
                     <div>
-                        <a class="btn btn-default submit" role="button">ログイン</a>
+                        <a class="btn btn-default submit pt-1" role="button">ログイン</a>
                         <a class="reset_pass" href="#">パスワードをお忘れですか？</a>
                     </div>
 
                     <div class="clearfix"></div>
 
                     <div class="separator">
-                        <p class="change_link">
-                            <a href="#signup" class="to_register">アカウント登録</a>
-                        </p>
-
-                        <div class="clearfix"></div>
-                        <br />
-
+                        <div class="clearfix"></div><br/>
                         <div>
-                            <h1>B-B テニストーナメント</h1>
-                            <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
-                        </div>
-                    </div>
-                </form>
-            </section>
-        </div>
-
-        <div id="register" class="animate form registration_form">
-            <section class="login_content">
-                <form>
-                    <h1>Create Account</h1>
-                    <div>
-                        <input type="text" class="form-control" placeholder="Username" required="" />
-                    </div>
-                    <div>
-                        <input type="email" class="form-control" placeholder="Email" required="" />
-                    </div>
-                    <div>
-                        <input type="password" class="form-control" placeholder="Password" required="" />
-                    </div>
-                    <div>
-                        <a class="btn btn-default submit" href="index.html">Submit</a>
-                    </div>
-
-                    <div class="clearfix"></div>
-
-                    <div class="separator">
-                        <p class="change_link">Already a member ?
-                            <a href="#signin" class="to_register"> Log in </a>
-                        </p>
-
-                        <div class="clearfix"></div>
-                        <br />
-
-                        <div>
-                            <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
-                            <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
+                            <h1>BB テニストーナメント</h1>
+                            <p>Copyright © 2002 BBテニストーナメント.<br> All rights reserved.</p>
                         </div>
                     </div>
                 </form>
