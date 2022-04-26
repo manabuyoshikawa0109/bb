7f540e3b7b19047c4fd9a5919b922619de718fad
吉川学 <yoshikawa.manabu@joydea.jp>
2022-04-26 21:26:20 +0900

    不要な処理を削除

--- resources/views/admin/commons/components/html/checkbox.blade.php
+++ resources/views/admin/commons/components/html/checkbox.blade.php
@@ -12,7 +12,6 @@
 // name属性が配列になっている場合に備えドット記法に変更
 $name = str_replace('[', '.', $fieldName);
 $name = str_replace(']', '', $name);
-info($name);
 @endphp
     <input id="" type="checkbox" class="{{ $class ?? null }}" name="{{ $fieldName }}" value="{{ $value ?? 1 }}" @if(old($name, $default ?? null) === ($value ?? 1)) checked @endif>
     @include('admin.commons.components.html.errors', ['fieldName' => $name])
