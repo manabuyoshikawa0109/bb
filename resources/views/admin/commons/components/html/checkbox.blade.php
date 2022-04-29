d97b9842efc3bac2920f7efa0d1d190e7d8de4b2
吉川学 <yoshikawa.manabu@joydea.jp>
2022-04-29 10:46:43 +0900

    チェックボックスコンポーネントを一部調整

--- resources/views/admin/commons/components/html/checkbox.blade.php
+++ resources/views/admin/commons/components/html/checkbox.blade.php
@@ -2,7 +2,7 @@
     チェックボックスコンポーネント
     $fieldName : name属性
     $class : クラス名
-    $value : チェックボックスの値
+    $value : チェックボックスの値(int型にする)
     $default : デフォルト値
     $name : $fieldNameをドット記法に変更したもの
 --}}
@@ -13,6 +13,6 @@
 $name = str_replace('[', '.', $fieldName);
 $name = str_replace(']', '', $name);
 @endphp
-    <input id="" type="checkbox" class="{{ $class ?? null }}" name="{{ $fieldName }}" value="{{ $value ?? 1 }}" @if(old($name, $default ?? null) === ($value ?? 1)) checked @endif>
+    <input id="" type="checkbox" class="{{ $class ?? null }}" name="{{ $fieldName }}" value="{{ $value ?? 1 }}" @if((int)old($name, $default ?? null) === ($value ?? 1)) checked @endif>
     @include('admin.commons.components.html.errors', ['fieldName' => $name])
 @endisset
