086b4f7ef3d97dcb1a37def15f03afea8cce09f1
吉川学 <yoshikawa.manabu@joydea.jp>
2022-05-20 22:33:33 +0900

    id属性に変な値が入っていたので一旦仮でnull指定 バリデーションメッセージ表示時にUIが一部崩れていたのでhtmlを調整

--- resources/views/admin/pages/event/row.blade.php
+++ resources/views/admin/pages/event/row.blade.php
@@ -14,31 +14,37 @@ for($minute = 0; $minute <= 45; $minute+=15){
 @endphp
 <tr>
     <td>
-        @include('admin.commons.components.html.text', ['fieldName' => "events[{$id}][name]", 'class' => 'event-name', 'default' => $event->name])
+        @include('admin.commons.components.html.text', ['id' => null, 'fieldName' => "events[{$id}][name]", 'class' => 'event-name', 'default' => $event->name])
     </td>
     <td>
-        @include('admin.commons.components.html.select', ['fieldName' => "events[{$id}][type_id]", 'options' => $type::$items, 'default' => $event->type_id])
+        @include('admin.commons.components.html.select', ['id' => null, 'fieldName' => "events[{$id}][type_id]", 'options' => $type::$items, 'default' => $event->type_id])
     </td>
     <td>
-        @include('admin.commons.components.html.number', ['fieldName' => "events[{$id}][applicants]", 'default' => $event->applicants])
+        @include('admin.commons.components.html.number', ['id' => null, 'fieldName' => "events[{$id}][applicants]", 'default' => $event->applicants])
     </td>
     <td>
         <div class="d-flex align-items-center">
-            @include('admin.commons.components.html.number', ['fieldName' => "events[{$id}][entry_fee]", 'default' => $event->entry_fee])
+            <div>
+                @include('admin.commons.components.html.number', ['id' => null, 'fieldName' => "events[{$id}][entry_fee]", 'default' => $event->entry_fee])
+            </div>
             <span class="ml-1">円</span>
         </div>
     </td>
     <td>
         <div class="d-flex align-items-center">
-            @include('admin.commons.components.html.select', ['fieldName' => "events[{$id}][start_hour]", 'options' => $hours, 'default' => $event->start_hour, 'label' => '時'])
+            <div>
+                @include('admin.commons.components.html.select', ['id' => null, 'fieldName' => "events[{$id}][start_hour]", 'options' => $hours, 'default' => $event->start_hour, 'label' => '時'])
+            </div>
             <span>：</span>
-            @include('admin.commons.components.html.select', ['fieldName' => "events[{$id}][start_minutes]", 'options' => $minutes, 'default' => $event->start_minutes, 'label' => '分'])
+            <div>
+                @include('admin.commons.components.html.select', ['id' => null, 'fieldName' => "events[{$id}][start_minutes]", 'options' => $minutes, 'default' => $event->start_minutes, 'label' => '分'])
+            </div>
         </div>
     </td>
     <td>
         {{-- イベントIDが正の数=DBに登録されているのでチェックボックスを表示 --}}
         @if($event->id && 0 < $event->id)
-            @include('admin.commons.components.html.checkbox', ['fieldName' => "events[{$id}][delete]", 'class' => 'icheck-red'])
+            @include('admin.commons.components.html.checkbox', ['id' => null, 'fieldName' => "events[{$id}][delete]", 'class' => 'icheck-red'])
         @else
             {{-- ajaxで削除する為のクラス名を付与（仮実装） --}}
             <i class="fas fa-trash-alt @if($event->id && 0 < $event->id) ajax-delete @endif" data-id="{{ $event->id }}"></i>
