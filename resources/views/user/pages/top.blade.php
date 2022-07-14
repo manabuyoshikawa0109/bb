8a536144ddb1b9163a18ff43cc233c1dfb338faf
吉川学 <yoshikawa.manabu@joydea.jp>
2022-07-14 22:30:41 +0900

    フロント/トップ 画像が一部表示されていなかったので修正

--- resources/views/user/pages/top.blade.php
+++ resources/views/user/pages/top.blade.php
@@ -107,7 +107,7 @@
     <!-- Carousel Start -->
     <div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="owl-carousel header-carousel position-relative">
-            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-1.jpg'>">
+            <div class="owl-carousel-item position-relative" data-dot="<img src='/assets/user/images/carousel-1.jpg'>">
                 <img class="img-fluid" src="/assets/user/images/carousel-1.jpg" alt="">
                 <div class="owl-carousel-inner">
                     <div class="container">
@@ -121,7 +121,7 @@
                     </div>
                 </div>
             </div>
-            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-2.jpg'>">
+            <div class="owl-carousel-item position-relative" data-dot="<img src='/assets/user/images/carousel-2.jpg'>">
                 <img class="img-fluid" src="/assets/user/images/carousel-2.jpg" alt="">
                 <div class="owl-carousel-inner">
                     <div class="container">
@@ -135,7 +135,7 @@
                     </div>
                 </div>
             </div>
-            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-3.jpg'>">
+            <div class="owl-carousel-item position-relative" data-dot="<img src='/assets/user/images/carousel-3.jpg'>">
                 <img class="img-fluid" src="/assets/user/images/carousel-3.jpg" alt="">
                 <div class="owl-carousel-inner">
                     <div class="container">
