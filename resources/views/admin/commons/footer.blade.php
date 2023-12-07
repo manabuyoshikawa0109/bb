3d682c0196e50d30a338c56392e72a8b149ae4fb
吉川学 <m.y.ichinishi.tennis@gmail.com>
2023-12-07 19:58:13 +0900

    フッターに表示している「年」が動的に変更されるよう対応

--- resources/views/admin/commons/footer.blade.php
+++ resources/views/admin/commons/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="page-footer">
-    <p class="mb-0">Copyright © 2021. All right reserved.</p>
+    <p class="mb-0">Copyright © {{ now()->format('Y') }}. All right reserved.</p>
 </footer>
