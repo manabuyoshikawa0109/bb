cf6a599b4f052b551c351e983bb5b33e81736a29
吉川学 <m.y.ichinishi.tennis@gmail.com>
2024-01-03 14:36:09 +0900

    テキストボックスコンポーネントを修正

--- resources/views/admin/commons/components/html/text.blade.php
+++ resources/views/admin/commons/components/html/text.blade.php
@@ -4,17 +4,11 @@
     $fieldName : name属性
     $class : クラス名
     $default : デフォルト値
-    $maxLength :　最大文字数
+    $maxLength : 最大文字数
     $placeholder : プレースホルダー
-    $name : $fieldNameをドット記法に変更したもの
 --}}
 
 @isset($fieldName)
-@php
-// name属性が配列になっている場合に備えドット記法に変更
-$name = str_replace('[', '.', $fieldName);
-$name = str_replace(']', '', $name);
-@endphp
-    <input id="{{ $id ?? null }}" type="text" class="form-control {{ $class ?? null }}" name="{{ $fieldName }}" value="{{ old($name, $default ?? null) }}" maxlength="{{ $maxLength ?? null }}" placeholder="{{ $placeholder ?? null }}">
-    @include('admin.commons.components.html.errors', ['fieldName' => $name])
+    <input id="{{ $id ?? null }}" type="text" class="form-control {{ $class ?? null }}" name="{{ $fieldName }}" value="{{ old($fieldName, $default ?? null) }}" maxlength="{{ $maxLength ?? null }}" placeholder="{{ $placeholder ?? null }}">
+    @include('admin.commons.components.html.errors', ['fieldName' => $fieldName])
 @endisset
