af92a2f45d7164c6e67cef864ca203ae6db62d40
吉川学 <m.y.ichinishi.tennis@gmail.com>
2024-01-03 17:20:21 +0900

    場所新規登録・編集画面 改行 ホーム画面のリンク先を追加

--- resources/views/admin/pages/place/input.blade.php
+++ resources/views/admin/pages/place/input.blade.php
@@ -18,7 +18,8 @@ if ($place->exists) {
     <div class="ps-3">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb mb-0 p-0">
-                <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                <li class="breadcrumb-item">
+                    <a href="{{ route('admin.home.index') }}"><i class="bx bx-home-alt"></i></a>
                 </li>
                 <li class="breadcrumb-item active" aria-current="page">{{ $pageTitle }}</li>
             </ol>
