1af9bf75793b944f7952ea5784bc22d061ff2876
吉川学 <m.y.ichinishi.tennis@gmail.com>
2024-01-07 21:11:50 +0900

    改行

--- resources/views/admin/pages/information/list.blade.php
+++ resources/views/admin/pages/information/list.blade.php
@@ -11,7 +11,8 @@ use App\Enums\ReleaseStatus;
     <div class="ps-3">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb mb-0 p-0">
-                <li class="breadcrumb-item"><a href="{{ route('admin.home.index') }}"><i class="bx bx-home-alt"></i></a>
+                <li class="breadcrumb-item">
+                    <a href="{{ route('admin.home.index') }}"><i class="bx bx-home-alt"></i></a>
                 </li>
                 <li class="breadcrumb-item active" aria-current="page">お知らせ一覧</li>
             </ol>
